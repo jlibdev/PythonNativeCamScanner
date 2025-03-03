6c149bcd95ddd14227646fbeb8a545d97f6f1aa8
Kyr Djan R. Marimon <kyrdjan.marimon@carsu.edu.ph>
2025-03-03 21:47:08 +0800

    add feature for selectign image in EditImageWidget

--- Main.py
+++ Main.py
@@ -1,47 +1,28 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QStackedWidget
 from PyQt6.QtGui import QFontDatabase , QFont, QIcon
-import stackwidgets.LandingWidget
-from stackwidgets.Widgets import CaptureWidget, EditImageWidget
-import treads.Watchers
-from utils import resource_path, save_path
-import treads
-import stackwidgets
+from stackwidgets.Widgets import LandingWidget , CaptureWidget, EditImageWidget
+from utils import resource_path
 
 class CamScammerApp(QStackedWidget):
     def __init__(self):
         super().__init__()
 
-        # STACKABLE WIDGETS / VIEWS
-        self.landingwidget = stackwidgets.LandingWidget.LandingWidget()
+        # Stackable Widgets 
+        self.landingwidget = LandingWidget()
         self.capture_widget = CaptureWidget()
         self.edit_image_widget = EditImageWidget()
 
-        # WIDGET INITIALIZATION
+
         self.addWidget(self.landingwidget)
         self.addWidget(self.capture_widget)
         self.addWidget(self.edit_image_widget)
 
-        # WATCHERS
-
-        # File Stream Watcher
-        self.file_stream_watcher = treads.Watchers.WatcherThread(save_path)
-        self.file_stream_watcher.start()
-
         # Signal Connections
         self.capture_widget.image_captured.connect(self.edit_image_widget.update_image)
         self.capture_widget.image_captured.connect(lambda: self.setCurrentWidget(self.edit_image_widget))
-
         self.landingwidget.switched.connect(self.capture_widget.toggle_camera)
-        self.file_stream_watcher.file_signal.connect(self.landingwidget.handle_file_change)
-
         self.resize(1280,720)
-    
-    def closeEvent(self, a0):
-        # Closing Threads
-        self.file_stream_watcher.stop()
-
-        return super().closeEvent(a0)
 
 def main():
 
